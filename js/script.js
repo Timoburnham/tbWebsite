228f82dd8b6b532ab152ffa25e963e11d46f023f
Timo Burnham <timoburnham@gmail.com>
2025-09-16 18:11:39 -0500

    added js functionality for moblie menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-//comment
+
+//mobile nav functionality
+
+const btnNav = document.querySelector(".btn-mobile-nav");
+const header = document.querySelector(".header");
+
+btnNav.addEventListener("click", function(){
+    header.classList.toggle("nav-open");
+})
+
+
