cac4bf87b8c9ea4083574984de827e1bab8a8e6c
Timo Burnham <timoburnham@gmail.com>
2025-09-18 10:09:53 -0500

    added functionality for expanding nav menu

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
-
 //mobile nav functionality
 
 const btnNav = document.querySelector(".btn-mobile-nav");
 const header = document.querySelector(".header");
 
-btnNav.addEventListener("click", function(){
-    header.classList.toggle("nav-open");
-})
+btnNav.addEventListener("click", function () {
+  header.classList.toggle("nav-open");
+});
 
+//nav expand functionality
 
+header.addEventListener("mouseenter", () => {
+  header.classList.add("expanded");
+  console.log("mouse entered");
+});
+
+header.addEventListener("mouseleave", () => {
+  header.classList.remove("expanded");
+});
+
+header.addEventListener("click", () => {
+  header.classList.toggle("expanded");
+});
